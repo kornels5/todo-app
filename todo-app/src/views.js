0b9b2ab30c04e3f63457705f8b8d5f56e911e592
Kornel Sroka <srokakornel@gmail.com>
2018-06-26 16:42:47 +0200

    added webpack to todo app

--- todo-app/src/views.js
+++ todo-app/src/views.js
@@ -1,64 +1,30 @@
-'use strict'
-
-// Fetch existing todos from localStorage
-const getSavedTodos = () => {
-    const todosJSON = localStorage.getItem('todos')
-
-    try {
-        return todosJSON ? JSON.parse(todosJSON) : []
-    } catch (e) {
-        return []
-    }
-}
-
-// Save todos to localStorage
-const saveTodos = (todos) => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-}
-
-// Remove todo by id
-const removeTodo = (id) => {
-    const todoIndex = todos.findIndex((todo) => todo.id === id)
-
-    if (todoIndex > -1) {
-        todos.splice(todoIndex, 1)
-    }
-}
-
-// Toggle the completed value for a given todo
-const toggleTodo = (id) => {
-    const todo = todos.find((todo) => todo.id === id)
-
-    if (todo) {
-        todo.completed = !todo.completed
-    }
-}
+import { getTodos, toggleTodo, removeTodo } from './todos'
+import { getFilters } from './filters'
 
 // Render application todos based on filters
-const renderTodos = (todos, filters) => {
+const renderTodos = () => {
     const todoEl = document.querySelector('#todos')
-    const filteredTodos = todos.filter((todo) => {
+    const filters = getFilters()
+    const filteredTodos = getTodos().filter((todo) => {
         const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
         const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-        
+
         return searchTextMatch && hideCompletedMatch
     })
-
     const incompleteTodos = filteredTodos.filter((todo) => !todo.completed)
 
     todoEl.innerHTML = ''
     todoEl.appendChild(generateSummaryDOM(incompleteTodos))
-    console.log(todos);
-    
-    if(filteredTodos.length === 0) {
-        const messageEl = document.createElement('p')
-        messageEl.textContent = 'No to-dos to show'
-        messageEl.classList.add('empty-message')
-        todoEl.appendChild(messageEl)
-    } else {
+
+    if (filteredTodos.length > 0) {
         filteredTodos.forEach((todo) => {
             todoEl.appendChild(generateTodoDOM(todo))
         })
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.classList.add('empty-message')
+        messageEl.textContent = 'There are no to-dos to show'
+        todoEl.appendChild(messageEl)
     }
 }
 
@@ -76,27 +42,25 @@ const generateTodoDOM = (todo) => {
     containerEl.appendChild(checkbox)
     checkbox.addEventListener('change', () => {
         toggleTodo(todo.id)
-        saveTodos(todos)
-        renderTodos(todos, filters)
+        renderTodos()
     })
 
     // Setup the todo text
     todoText.textContent = todo.text
     containerEl.appendChild(todoText)
 
-    //Setup container
+    // Setup container
     todoEl.classList.add('list-item')
     containerEl.classList.add('list-item__container')
     todoEl.appendChild(containerEl)
 
     // Setup the remove button
     removeButton.textContent = 'remove'
-    removeButton.classList.add('button', 'button-text')
+    removeButton.classList.add('button', 'button--text')
     todoEl.appendChild(removeButton)
     removeButton.addEventListener('click', () => {
         removeTodo(todo.id)
-        saveTodos(todos)
-        renderTodos(todos, filters)
+        renderTodos()
     })
 
     return todoEl
@@ -110,3 +74,5 @@ const generateSummaryDOM = (incompleteTodos) => {
     summary.textContent = `You have ${incompleteTodos.length} todo${plural} left`
     return summary
 }
+
+export { generateTodoDOM, renderTodos, generateSummaryDOM }
